aec6ec653edc70bbd30f54417be0e8018f07ff74
Ivan Koshkin <ivan@koshkin.me>
2011-02-22 15:41:22 +0300

    tmp

--- src/rdbl.erl
+++ src/rdbl.erl
@@ -131,9 +131,7 @@ simplify_page(Body0, Ctx, DefaultContentType) ->
 				)
 			),
 			OptimumRef = get_max_score_ref(ScoredTree),
-%%			ContentBody = clean_scores(find_node(OptimumRef, ScoredTree)),
-%ContentBody=clean_scores(score_tree(init_scores(TreeBody))),
-ContentBody=TreeBody,
+			ContentBody = clean_scores(find_node(OptimumRef, ScoredTree)),
 			% do something if Content body is started with <td> or <tr>
 			case ContentBody of
 				{<<"td">>, _, CellContent} -> Out = CellContent;
